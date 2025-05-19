fe9fb48d3ceaff8a58820fe66d22a55f85151cb9
cameron Saunders <cameron.saunders80@qmail.cuny.edu>
2025-05-19 19:17:03 -0400

    added link

--- src/app/components/Menubar.jsx
+++ src/app/components/Menubar.jsx
@@ -1,14 +1,20 @@
+import Link from 'next/link'
+
 export default function Menubar() {
     return (
       <>
       <ul>
-        <li>Home</li>
-        <li>Midterm Part I</li>
-        <li>Midterm Part II</li>
+        <li>
+          <Link href='/'>Home</Link>
+        </li>
+        <li>
+          <a href='https://expense-six-livid.vercel.app' target='_blank'>Midterm Part I - Expense</a>
+        </li>
+        <li>
+          <a href='https://ophth-website-jxcx.vercel.app' target='_blank'>Midterm Part II - Site</a>
+        </li>
       </ul>
       
         </>
     );
   }
-
-  
